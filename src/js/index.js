876ec54ad34fdd56755fefa135318af90b0e645f
Rasmus Gerdin <me@rasmusgerdin.com>
2018-05-02 03:25:27 +0200

    more stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,31 @@
 const discordTip = document.querySelectorAll('#discord');
+const thecodeTip = document.querySelectorAll('#thecode');
+const hhughesTip = document.querySelectorAll('#hhughes');
+
 tippy(discordTip, {
     arrow: true,
     arrowType: 'small',
     animation: 'shift-away',
     placement: 'left',
+    touchHold: true
+});
+
+tippy(thecodeTip, {
+    interactive: true,
+    interactiveBorder: 2,
+    theme: 'rgerdin',
+    html: document.querySelector('#thecodequote'),
+    animation: 'shift-away',
+    placement: 'bottom',
+    touchHold: true
+});
+
+tippy(hhughesTip, {
+    interactive: true,
+    interactiveBorder: 2,
+    theme: 'rgerdin',
+    html: document.querySelector('#hhughesquote'),
+    animation: 'shift-away',
+    placement: 'bottom',
     touchHold: true
 });
